9d448f33927ec78beb22f38cafef024c15c340d5
stephen-shelton <stephenshelton777@gmail.com>
2016-12-27 23:49:01 -0800

    fixed port on server to work with heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message.js');
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
